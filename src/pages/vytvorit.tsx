db3ed3fca02c2fee5157747d399b231040f94699
andrejbinovsky <binovsky.andrej@gmail.com>
2022-10-15 23:14:22 +0200

    add auth protection to /vytvorit

--- src/pages/vytvorit.tsx
+++ src/pages/vytvorit.tsx
@@ -2,21 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { MainLayout } from '@components/big/MainLayout'
 import { ProductCreationForm } from '@components/forms/ProductCreationForm'
+import { AuthLayout } from '@components/big/AuthLayout'
 const Vytvorit: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>Vytvořiť</title>
-      </Head>
+    <AuthLayout>
+      <div>
+        <Head>
+          <title>Vytvořiť</title>
+        </Head>
 
-      <main>
-        <MainLayout>
-          <div className="flex items-center justify-center h-full w-full">
-            <ProductCreationForm />
-          </div>
-        </MainLayout>
-      </main>
-    </div>
+        <main>
+          <MainLayout>
+            <div className="flex items-center justify-center h-full w-full">
+              <ProductCreationForm />
+            </div>
+          </MainLayout>
+        </main>
+      </div>
+    </AuthLayout>
   )
 }
 
