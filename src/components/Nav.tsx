943b3d7b821cdef16ed163e862083be96d55fb89
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2023-05-27 14:15:05 +0200

    .

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,11 +5,13 @@ import Image from 'next/image'
 // import {signIn, signOut, useSession, getProviders} from 'next-auth/client';
 
 const Nav = () => {
+    const s  = 10;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/">
         <Image src={"/next.svg"} alt="aas" width={30} height={30}></Image>
       </Link>
+      <h1>{s}</h1>
     </nav>
   )
 }
